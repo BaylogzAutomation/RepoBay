451e7da6447f24e73f7aafd895c4adf4fd8a0a51
unknown <BaylogzAutomation>
2024-06-17 13:00:50 -0400

    first fruit

---
+++ app.js
@@ -0,0 +1,23 @@
+// const day = 'Thursday'
+
+// const position = '4th'
+
+// const output = 'Today is ' + day + '.' + ' It is the ' + position + ' day of the week.'
+
+// console.log(output)
+
+// const addition = 40%2
+
+// console.log(addition)
+
+// const age = 18
+// const age1 = 20
+
+// console.log(age1>=age)
+ 
+const userName = 'Victory'
+const age = 12
+
+const logic = age === 12 || userName === 'Victory'
+
+console.log(!logic)
