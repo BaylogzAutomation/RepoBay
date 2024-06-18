dc51c9e563c846fab8d2df3a9f7e9a3cc1796159
unknown <BaylogzAutomation>
2024-06-18 15:16:15 -0400

    baylogz

---
+++ Ass2.js
@@ -0,0 +1,16 @@
+
+// let star = 1 //start point
+// while(star <=10) {
+//     if(star===1)
+//         console.log (star + " star")
+//     else 
+//         console.log(star + ' stars')
+//     star = star + 1
+// }
+
+
+for (let oddNumbers = 1; oddNumbers <= 20; oddNumbers++) {
+  if (oddNumbers % 2) {
+    console.log(oddNumbers)
+  }
+}
