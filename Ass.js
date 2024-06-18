542e58f99553c9399a84524e7caeeedccedc54d7
unknown <BaylogzAutomation>
2024-06-18 13:45:46 -0400

    first commit

---
+++ Ass.js
@@ -0,0 +1,12 @@
+
+const number = 53
+if(number&4){
+    console.log('number is even')
+} else {
+    console.log('number is odd')
+}
+
+
+
+
+
--- app.js
+++ app.js
@@ -15,9 +15,60 @@
 
 // console.log(age1>=age)
  
-const userName = 'Victory'
-const age = 12
+// const userName = 'Victory'
+// const age = 12
 
-const logic = age === 12 || userName === 'Victory'
+// const logic = age === 12 || userName === 'Victory'
 
-console.log(!logic)
+// console.log(logic)
+
+// const age = 90
+
+// if(age>=18 && age<=70) {
+//     console.log('you are eligible to vote')
+// } else if(age>70) {
+//     console.log('you are too old to vote')
+// } else {
+//     console.log('you are too young to vote')
+// }
+
+// const day = 'Friday'
+
+// if(day==='Friday') {
+//     console.log('TGIF')
+// } else if(day==='Saturday') {
+//     console.log('Yeh! The weekend is here!')
+// } else if(day==='Sunday') {
+//     console.log('Happy Sunday!')
+// } else {
+//     console.log('Go to work')
+// }
+    
+// switch(day){
+//     case 'Friday':
+//         console.log('TGIF')
+//         break
+//     case 'Saturday':
+//         console.log('Yeh! The weekend is here!')
+//         break
+//     case 'Sunday':
+//         console.log('Happy Sunday!')
+//         break
+//     default:
+//         console.log('Go to work')
+//         break
+// }
+
+// let star = 1 //start point
+// while(star <=100){
+//     if(star===1)
+//         console.log(star + ' star')
+//     else
+//         console.log(star + ' stars')
+//     star = star + 1
+// }
+
+
+
+
+
